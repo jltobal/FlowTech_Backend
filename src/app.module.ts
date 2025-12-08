855a7c967cce70de8ccd46f03bc2d9215eeb3170
jltobal <ziba.tobal@gmail.com>
2025-12-08 12:45:16 -0300

    bbdd read user test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { InteraccionesModule } from './interacciones/interacciones.module';
+import { AuthModule } from './auth/auth.module';
+import { UsuarioService } from './usuario/usuario.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/grietinder'),
-    AuthModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/GRIETINDER_DB'),
     UsuarioModule,
-    InteraccionesModule
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private readonly usuarioService: UsuarioService) {}
+
+  async onModuleInit() {
+    console.log('--------------------------------------------------');
+    console.log('🚀 Ejecutando prueba de conexión con findAll()');
+    await this.usuarioService.findAll();
+    console.log('✨ Prueba de conexión finalizada. Revise la consola arriba.');
+    console.log('--------------------------------------------------');
+  }
+}
+
+/* MongooseModule.forRoot('mongodb://localhost:27017/GRIETINDER_DB'), */
--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsuarioService } from './usuario.service';
 import { UsuarioController } from './usuario.controller';
 import { Usuario, UsuarioSchema } from './schemas/usuario.schema';
-import { Interacciones, InteraccionesSchema } from '../interacciones/schemas/interacciones.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Usuario.name, schema: UsuarioSchema }, {name: Interacciones.name, schema: InteraccionesSchema}])],
+  imports: [
+    MongooseModule.forFeature([{ name: Usuario.name, schema: UsuarioSchema }]),
+  ],
   providers: [UsuarioService],
-  controllers: [UsuarioController]
+  controllers: [UsuarioController],
+  exports: [UsuarioService],
 })
 export class UsuarioModule {}
